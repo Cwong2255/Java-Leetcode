dea4d1f2bab1d0bcc2788835a94c6a89b03d55d4
Cwong2255 <70491160+Cwong2255@users.noreply.github.com>
2022-10-14 12:30:36 -0400

    LeetCode 41. First Missing Positive

--- Practice.java
+++ Practice.java
@@ -21,9 +21,34 @@ import java.util.regex.Pattern;
 class Practice {
 
     public static void main(String[] args) throws Exception {
-        int[] arr = { 10, 2, -5 };
-        System.out.println(maxVowels("leetcode", 3));
+        int[] arr = { 7, 8, 9, 11, 12 };
+        System.out.println(firstMissingPositive(arr));
+    }
 
+    public static int firstMissingPositive(int[] nums) {
+        boolean containsOne = false;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                containsOne = true;
+            } else if (nums[i] > nums.length || nums[i] <= 0) {
+                nums[i] = 1;
+            }
+        }
+        if (!containsOne) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return nums.length + 1;
     }
 
     public static int maxVowels(String s, int k) {
