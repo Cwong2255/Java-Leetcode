6f5b0d97994227476a74addd46032c5af215bb0a
Cwong2255 <70491160+Cwong2255@users.noreply.github.com>
2022-10-20 12:07:02 -0400

    LeetCode 198. House Robber

--- Practice.java
+++ Practice.java
@@ -21,9 +21,23 @@ import java.util.regex.Pattern;
 class Practice {
 
     public static void main(String[] args) throws Exception {
-        int[] arr = { 1, 2, 5 };
-        String[] strArr = { "i", "love", "leetcode", "i", "love", "coding" };
-        System.out.println(coinChange(arr, 11));
+        int[] arr = { 1, 2, 3, 1 };
+        System.out.println(rob(arr));
+    }
+
+    public static int rob(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        } else if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int[] dp = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);
+        }
+        return dp[nums.length];
     }
 
     public static int coinChange(int[] coins, int amount) {
