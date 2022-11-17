79a7ef0e0ea35b0d3d71907bc9f04fa85c54cf03
Cwong2255 <70491160+Cwong2255@users.noreply.github.com>
2022-11-16 19:02:58 -0500

    LeetCode DP Study Plan Day 1

--- Practice.java
+++ Practice.java
@@ -22,7 +22,50 @@ class Practice {
 
     public static void main(String[] args) throws Exception {
         int[] arr = { 1, 2, 3, 1, 2, 3 };
-        System.out.println(containsNearbyDuplicate2(arr, 2));
+        System.out.println(tribonacci(25));
+    }
+
+    public static int tribonacci(int n) {
+        if (n < 2) {
+            return n;
+        } else if (n == 2) {
+            return 1;
+        }
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[dp.length - 1];
+    }
+
+    public static int fibonacci(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int dp[] = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[dp.length - 1];
+    }
+
+    public static int factorial(int n) {
+        if (n < 2) {
+            return 1;
+        }
+        int dp[] = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = i * dp[i - 1];
+        }
+        return dp[dp.length - 1];
     }
 
     public static boolean containsNearbyDuplicate2(int[] nums, int k) {
